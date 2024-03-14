051d88bb3236811086ee5a92513e0c1787b893f7
NasrulloDev <ismoilzanovnasrullo@gmail.com>
2024-03-14 08:37:21 +0500

    Initial Commit

---
+++ HotProspects/HotProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectsApp.swift
+//  HotProspects
+//
+//  Created by Насрулло Исмоилжонов on 14/03/24.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
