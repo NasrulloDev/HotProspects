b1bf9e901da863a8362062afdb8c724d0b28b2af
NasrulloDev <ismoilzanovnasrullo@gmail.com>
2024-03-15 21:10:20 +0500

    contextMenu
    Creating context menus

--- HotProspects/ContentView.swift
+++ HotProspects/ContentView.swift
@@ -8,15 +8,30 @@
 import SwiftUI
 
 struct ContentView: View {
-    
+    @State private var backgroundColor = Color.red
     
     var body: some View {
-        Image(.nikeLogo47A65A59D5SeeklogoCom)
-            .interpolation(.none)
-            .resizable()
-            .scaledToFit()
-            .background(.green)
-            
+        VStack {
+                    Text("Hello, World!")
+                        .padding()
+                        .background(backgroundColor)
+
+                    Text("Change Color")
+                        .padding()
+                        .contextMenu {
+                            Button("Red", systemImage: "checkmark.circle.fill") {
+                                backgroundColor = .red
+                            }
+
+                            Button("Green") {
+                                backgroundColor = .green
+                            }
+
+                            Button("Blue") {
+                                backgroundColor = .blue
+                            }
+                        }
+                }
     }
 }
 
