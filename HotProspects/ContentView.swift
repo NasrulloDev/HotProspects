111d1d675ccbb349d20d1ce0f196046f355203c8
NasrulloDev <ismoilzanovnasrullo@gmail.com>
2024-03-16 23:01:30 +0500

    swipeActions
    Adding custom row swipe actions to a List

--- HotProspects/ContentView.swift
+++ HotProspects/ContentView.swift
@@ -12,7 +12,20 @@ struct ContentView: View {
     
     var body: some View {
         VStack {
-                    
+            List{
+                Text("Taylor Swift")
+                    .swipeActions {
+                        Button("Delete", systemImage: "minus.circle", role: .destructive){
+                            print("Delete")
+                        }
+                    }
+                    .swipeActions(edge: .leading){
+                        Button("Pin", systemImage: "pin"){
+                            print("Pinning")
+                        }
+                        .tint(.orange)
+                    }
+            }
         }
     }
 }
