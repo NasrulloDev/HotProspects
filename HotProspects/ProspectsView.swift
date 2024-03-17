6863e21cdfad8e353fd2033a62d8d4abe4a858f4
NasrulloDev <ismoilzanovnasrullo@gmail.com>
2024-03-17 19:56:33 +0500

    filtering query
    Dynamically filtering our SwiftData query

--- HotProspects/ProspectsView.swift
+++ HotProspects/ProspectsView.swift
@@ -29,14 +29,34 @@ struct ProspectsView: View {
     }
     var body: some View {
         NavigationStack{
-            Text("People: \(prospects.count)")
-                .navigationTitle(title)
-                .toolbar{
-                    Button("Scan", systemImage: "qrcode.viewfinder"){
-                        let prospect = Prospect(name: "Nasrullo", emailAddress: "nasrullo@gmail.com", isContacted: false)
-                        modelContext.insert(prospect)
-                    }
+            List(prospects){ prospect in
+                VStack(alignment: .leading) {
+                    Text(prospect.name)
+                        .font(.headline)
+                    
+                    Text(prospect.emailAddress)
+                        .foregroundStyle(.secondary)
                 }
+            }
+            .navigationTitle(title)
+            .toolbar{
+                Button("Scan", systemImage: "qrcode.viewfinder"){
+                    let prospect = Prospect(name: "Nasrullo", emailAddress: "nasrullo@gmail.com", isContacted: false)
+                    modelContext.insert(prospect)
+                }
+            }
+        }
+    }
+    
+    init(filter: FilterType) {
+        self.filter = filter
+        
+        if filter != .none {
+            let showContactedOnly = filter == .contacted
+            
+            _prospects = Query(filter: #Predicate {
+                $0.isContacted == showContactedOnly
+            }, sort: [SortDescriptor(\Prospect.name)])
         }
     }
 }
