eb40a50f1a432a7d0e5d7f14f73d95a483985974
NasrulloDev <ismoilzanovnasrullo@gmail.com>
2024-03-17 19:32:30 +0500

    TabView()
    Building our tab bar

--- HotProspects/ContentView.swift
+++ HotProspects/ContentView.swift
@@ -6,19 +6,31 @@
 //
 
 import SwiftUI
-import SamplePackage
 
 struct ContentView: View {
-    let possibleNumbers = 1...60
-    
-    var results: String {
-        let selected = possibleNumbers.random(7).sorted()
-        let strings = selected.map(String.init)
-        return strings.formatted()
-    }
     
     var body: some View {
-        Text(results)
+        TabView{
+            ProspectsView(filter: .none)
+                .tabItem {
+                    Label("Everyone", systemImage: "person.3")
+                }
+            
+            ProspectsView(filter: .contacted)
+                .tabItem {
+                    Label("Contacted", systemImage: "checkmark.circle")
+                }
+            
+            ProspectsView(filter: .uncontacted)
+                .tabItem {
+                    Label("Uncontacted", systemImage: "questionmark.diamond")
+                }
+            
+            MeView()
+                .tabItem {
+                    Label("Me", systemImage: "person.crop.square")
+                }
+        }
     }
 }
 
---
+++ HotProspects/MeView.swift
@@ -0,0 +1,18 @@
+//
+//  MeView.swift
+//  HotProspects
+//
+//  Created by Насрулло Исмоилжонов on 17/03/24.
+//
+
+import SwiftUI
+
+struct MeView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    MeView()
+}
