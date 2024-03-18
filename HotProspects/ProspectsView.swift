9dec3413d2d16bee16c72917aab2301a5d853bdf
NasrulloDev <ismoilzanovnasrullo@gmail.com>
2024-03-18 06:11:39 +0500

    CodeScannerView
    Scanning QR codes with SwiftUI

--- HotProspects/ProspectsView.swift
+++ HotProspects/ProspectsView.swift
@@ -4,6 +4,7 @@
 //
 //  Created by Насрулло Исмоилжонов on 17/03/24.
 //
+import CodeScanner
 import SwiftData
 import SwiftUI
 
@@ -14,6 +15,7 @@ struct ProspectsView: View {
     
     @Environment(\.modelContext) var modelContext
     @Query(sort: \Prospect.name) var prospects: [Prospect]
+    @State private var isShowingScanner = false
     
     let filter: FilterType
     
@@ -41,9 +43,11 @@ struct ProspectsView: View {
             .navigationTitle(title)
             .toolbar{
                 Button("Scan", systemImage: "qrcode.viewfinder"){
-                    let prospect = Prospect(name: "Nasrullo", emailAddress: "nasrullo@gmail.com", isContacted: false)
-                    modelContext.insert(prospect)
+                    isShowingScanner = true
                 }
+            }
+            .sheet(isPresented: $isShowingScanner){
+                CodeScannerView(codeTypes: [.qr], simulatedData: "Nasrullo\nismoiljonov@mail.ru", completion: handleScan)
             }
         }
     }
@@ -57,6 +61,22 @@ struct ProspectsView: View {
             _prospects = Query(filter: #Predicate {
                 $0.isContacted == showContactedOnly
             }, sort: [SortDescriptor(\Prospect.name)])
+        }
+    }
+    
+    func handleScan(result: Result<ScanResult, ScanError>) {
+        isShowingScanner = false
+        
+        switch result {
+        case .success(let result):
+            let details = result.string.components(separatedBy: "\n")
+            guard details.count == 2 else { return }
+            
+            let person = Prospect(name: details[0], emailAddress: details[1], isContacted: false)
+            modelContext.insert(person)
+            
+        case .failure(let error):
+            print("Scanning failde: \(error.localizedDescription)")
         }
     }
 }
